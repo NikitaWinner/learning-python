de52867aabf30ffdb6a487d831be9350bc69ec16
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-26 01:47:10 +0300

    Update task_3_3.py

--- Homework 3/task_3_3.py
+++ Homework 3/task_3_3.py
@@ -14,6 +14,7 @@ def thesaurus(*args) -> dict:
 
 print(thesaurus('Муся', "Иван", "Мария", "Петр", "Илья", 'Пашган'))
 
+
 def thesaurus_2(*args) -> dict:
     """ Функция, принимает в качестве аргументов имена сотрудников
     и возвращает словарь, в котором ключи — первые буквы имён,
@@ -24,6 +25,7 @@ def thesaurus_2(*args) -> dict:
         dict_out[name[0]] = list(filter(lambda el: el.startswith(name[0]), args))
     return dict(sorted(dict_out.items()))
 
+
 print(thesaurus_2("Никита", 'Витя', "Слава", "Нина", "Валерия", 'Юра'))
 
 
@@ -32,4 +34,6 @@ def thesaurus_bdsm(*args) -> dict:
     и возвращает словарь, в котором ключи — первые буквы имён,
     а значения — списки, содержащие имена, начинающиеся с соответствующей буквы. """
     return dict(sorted({name[0].title() : list(filter(lambda el: el[0] == name[0], args)) for name in args}.items(), key=lambda el: el[0]))
+
+
 print(thesaurus_bdsm('dark','dariy', "bunny", "Puppy", "miss", "rope", 'ridic', 'Mr.Krabs'))
