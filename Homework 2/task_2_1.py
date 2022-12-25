ba8aca9f739fc4aecad939f54fb025b42a06273d
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-26 01:38:16 +0300

    Update task_2_1.py

--- Homework 2/task_2_1.py
+++ Homework 2/task_2_1.py
@@ -1,9 +1,7 @@
-result_1 = 15 * 3
-result_2 = 15 / 3
-result_3 = 15 // 2
-result_4 = 15 ** 3
+def print_type_object(*objects):
+    """Выводит в консоль типы переданых объектов в параметр objects"""
 
-print('Тип объекта переменной result_1 - ', result_1.__class__.__name__)
-print('Тип объекта переменной result_2 - ', result_2.__class__.__name__)
-print('Тип объекта переменной result_3 - ', result_3.__class__.__name__)
-print('Тип объекта переменной result_4 - ', result_4.__class__.__name__)
+    for object in objects:
+        print(f'Тип объекта: {object} -> {object.__class__.__name__}')
+
+print_type_object(15 * 3, 15 / 3, 15 // 2, 15 ** 3, 'hello', bool(1), [], ())
