b015022fdd61b7cbdd5d71096cee1748385a75ba
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-27 01:03:37 +0300

    Add files via upload

---
+++ Homework 4/task_4_2.py
@@ -0,0 +1,47 @@
+import requests
+
+def return_index(string: str, char_code: str, entry='first') -> int:
+    """
+    Функция принимает строку и подстроку возвращает индекс первого
+    или последнего вхождения подстроки char_code в строку string
+    в зависимости от установленого флага edge
+    :param string: строка, в которой будет проводиться поиск индекса
+    :param char_code: подстрока, индекс которой будет возвращаться
+    :param entry: флаг, который определяет индекс первого или последнего
+    вхождения будет возвращен. Если start - первое вхождение
+                               Если end -  последнее вхождение (indx + 1)
+    :return: индекс подстроки
+    """
+
+    if entry == 'first':
+        return int(string.find(char_code))
+    elif entry == 'last':
+        return int(string.find(char_code)) + len(char_code) - 1
+    else:
+        pass
+
+
+def currency_rates(code: str) -> float:
+    """возвращает курс валюты `code` по отношению к рублю"""
+    data = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    data = data.text.split('ID')
+    for element in data:
+        index_start = return_index(element, '<CharCode>', entry='last') + 1
+        index_end = return_index(element, '</CharCode>', entry='first')
+        char_code = element[index_start:index_end]
+        if code.startswith(char_code):
+            index_start = return_index(element, '<Value>', entry='last') + 1
+            index_end = return_index(element, '</Value>', entry='first')
+            value = float(element[index_start:index_end].replace(',', '.'))
+            index_start = return_index(element, '<Nominal>', entry='last') + 1
+            index_end = return_index(element, '</Nominal>', entry='first')
+            nominal = float(element[index_start:index_end])
+            if nominal != float('1'):
+                return round(value / nominal, 2)
+            else:
+                return round(value, 2)
+
+
+
+print(currency_rates("AMD"))
+print(currency_rates("noname"))
---
+++ Homework 4/task_4_4-5.py
@@ -0,0 +1,4 @@
+import sys
+import utils
+
+exit(utils.currency_rates_adv(sys.argv))
