4a0bd9f688c749ecf8aeab7b301e1e26d3dd7524
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-27 20:41:40 +0300

    Update task_5_2.py

--- Homework 5/task_5_2.py
+++ Homework 5/task_5_2.py
@@ -13,5 +13,5 @@ generator = odd_nums(n)
 for _ in range(1, n + 1, 2):
     print(next(generator))
 
-print(f'Тип объекта generator -> {type(generator)}')
+print(f'Тип возвращаемого значения функции odd_nums -> {type(generator)}')
 #  next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
