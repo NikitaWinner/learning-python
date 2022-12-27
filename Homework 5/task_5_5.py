68b939e849c343dba092b7f2ff7d964123dfaddf
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-27 20:39:05 +0300

    Update task_5_5.py

--- Homework 5/task_5_5.py
+++ Homework 5/task_5_5.py
@@ -1,5 +1,7 @@
-def get_uniq_numbers(src: list):
-    """ Функция возвращает генератор, отдающий элементы,
+import typing
+
+def get_uniq_numbers_1(src: list) -> list:
+    """ Функция возвращает список, отдающий элементы,
     которые не имеют повторений в переданном списке
     :param src: список со значениями
     :return: генератор с уникальными значениями """
@@ -15,6 +17,21 @@ def get_uniq_numbers(src: list):
     return [el for el in src if el in set_out]
 
 
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print(*get_uniq_numbers_1(src))
+
+
+def get_uniq_numbers_2(src: list) -> typing.Generator:
+    """ Функция возвращает генератор, который отдаёт те элементы,
+    которые больше предыдущего в переданном списке
+    :param src: список со значениями
+    :return: генератор с уникальными значениями """
+
+    dict_temp = {key: 0 for key in src}
+    for key in src:
+        dict_temp[key] += 1
+    return (key for key in dict_temp if dict_temp[key] == 1)
+
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-print(*get_uniq_numbers(src))
+print(*get_uniq_numbers_2(src))
