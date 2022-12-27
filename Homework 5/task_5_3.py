4166710caf749c2c9a4d328adef91729824969b7
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2022-12-27 20:11:08 +0300

    Update task_5_3.py

--- Homework 5/task_5_3.py
+++ Homework 5/task_5_3.py
@@ -58,23 +58,4 @@ generator = check_gen_3(tutors, klasses)
 print(f'Тип возвращаемого значения функции check_gen_3 -> {type(generator)}')
 # for _ in range(len(tutors)):
 #     print(next(generator))
-# next(generator)  # если раскомментировать, то в traceback упадёт исключение StopIteration
-
-
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Вася', 'Егор']
-klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б']
-
-
-def check_gen_2(tutors: list, klasses: list) -> typing.Generator:
-    """ Функция возвращает генератор, который отдаёт
-     элементы ввиде кортежей (<tutors>, <klasses>) """
-
-    for i in range(len(tutors)):
-        yield tutors[i], klasses[i] if i < len(klasses) else None
-
-# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
-
-generator = check_gen_2(tutors, klasses)
-print(f'Тип объекта generator -> {type(generator)}')
-for _ in range(len(tutors)):
-    print(next(generator))
+# next(generator)  # если раскомментиров
