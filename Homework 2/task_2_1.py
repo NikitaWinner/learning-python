48c07c6f954f134450d2368a2c7f92dbc4e9598e
NikitaWinner <93407105+NikitaWinner@users.noreply.github.com>
2023-01-28 21:37:59 +0300

    Delete Homework 2 directory

--- Homework 2/task_2_1.py
+++
@@ -1,7 +0,0 @@
-def print_type_object(*objects):
-    """Выводит в консоль типы переданых объектов в параметр objects"""
-
-    for object in objects:
-        print(f'Тип объекта: {object} -> {object.__class__.__name__}')
-
-print_type_object(15 * 3, 15 / 3, 15 // 2, 15 ** 3, 'hello', bool(1), [], ())
